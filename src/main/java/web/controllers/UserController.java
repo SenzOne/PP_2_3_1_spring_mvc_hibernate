ffb56ac5e5df20eb22857c887087ff34413bab03
Senz One <sem63sam@yandex.ru>
2024-02-15 01:08:38 +0400

    all fix

--- src/main/java/web/controllers/UserController.java
+++ src/main/java/web/controllers/UserController.java
@@ -4,10 +4,7 @@ package web.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import web.models.User;
 import web.service.UserService;
 
@@ -29,7 +26,7 @@ public class UserController {
         return "users";
     }
 
-    @GetMapping("/new")
+    @RequestMapping("/new")
     public String CreateUserForm(@ModelAttribute("user") User user) {
         return "userCreate";
     }
@@ -52,74 +49,10 @@ public class UserController {
         return "userUpdate";
     }
 
-//    @PostMapping("/updateUser")
-//    public String updateUser(@ModelAttribute("user") User user) {
-//        userService.update(user);
-//        return "redirect:/users";
-//    }
 
-
-//    @GetMapping()
-//    public String showAllUser(Model model){
-//        model.addAttribute("users", userService.getAllUsers());
-//        return "allUsers";
-//    }
-//
-//    @GetMapping("/addNewUser")
-//    public String addNewUser(Model model){
-//        model.addAttribute("user", new User());
-//        return "user-info";
-//    }
-//
-//
-//    @RequestMapping("/saveUser")
-//    public String saveUser(@ModelAttribute("user") User user) {
-//        userService.save(user);
-//        return "redirect: /allUsers";
-//    }
-
-//    @GetMapping()
-//    public String allUsers(Model model) {
-//        model.addAttribute("users", userService.getAllUsers());
-//        return "/allUsers";
-//    }
-//
-//
-//    @GetMapping("/{id}")
-//    public String show(@PathVariable("id") Long id, Model model) {
-//        User user = userService.getUser(id);
-//        model.addAttribute("user", user);
-//        return "user-info";
-//    }
-//
-//    @GetMapping("/new")
-//    public String newUser(@ModelAttribute("user") User user) {
-//        return "/new";
-//    }
-//
-//    @PostMapping()
-//    public String create(@ModelAttribute("user") User user) {
-//        userService.save(user);
-//        return "redirect: /allUsers";
-//    }
-//
-//    @GetMapping("/{id}/edit")
-//    public String edit(Model model, @PathVariable("id") Long id) {
-//        model.addAttribute("user", userService.getUser(id));
-//        return "edit";
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public String update(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
-//        userService.update(id, user);
-//        return "redirect: /allUsers";
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public String delete(@PathVariable("id") Long id) {
-//        userService.delete(id);
-//        return "redirect: /allUsers";
-//    }
-
-
+    @PostMapping("/updateUser")
+    public String postEditUserForm(@ModelAttribute("user") User user) {
+        userService.update(user);
+        return "redirect:/users";
+    }
 }
